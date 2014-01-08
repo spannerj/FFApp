a59699ee9a3df5b4048568a84e7b53365b6cf700
git config --global user.name Spencer <spencer.jago.register@gmail.com>
2014-01-08 21:21:12 +0000

    init

---
+++ config.ru
@@ -0,0 +1,3 @@
+require File.dirname(__FILE__) + '/telegraph'
+
+run FFApp.new
---
+++ telegraph.rb
@@ -0,0 +1,35 @@
+require 'sinatra/base'
+require "sinatra/reloader"
+require 'pg'
+
+class FFApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+	before do
+		$conn = PGconn.connect('localhost', '5432', '', '', 'lrff', 'postgres', '')
+		$teams = $conn.exec("select distinct team from players order by 1")
+		#p params
+	end
+
+	get '/players' do
+		if not params[:team].nil? #team
+			team = params[:team]
+	  		$res = $conn.exec_params("select player_id, player_name, player_position, player_value, team, player_link 
+	  							  	from players where team = $1 order by 1;", [team])
+	  	end
+
+	  	if not params[:pos].nil? #all players
+	  		pos = params[:pos]
+		  	$res = $conn.exec_params("select player_id, player_name, player_position, player_value, team, player_link 
+		  	 						  from players where player_position = $1 order by 1;", [pos])
+		end
+		  	 	
+	  	if (params[:pos].nil? and params[:team].nil?)
+			$res = $conn.exec("select player_id, player_name, player_position, player_value, team, player_link from players order by 1;")
+	  	end
+
+	 	erb :telegraph
+	end
+end
